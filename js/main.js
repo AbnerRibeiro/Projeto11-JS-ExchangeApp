4e721301dbdd157d2174c28f18bd9813e9c3d454
Abner Ribeiro <abnergoulart@yahoo.com.br>
2019-07-08 22:12:54 -0300

    terminando js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 // jQuery
 $(document).ready(function(){
-	const api = 'https://currencydatafeed.com/api/data.php?token=fs3yuzrl7k8nv1ia2jf1&currency=BRL/USD'; 
-	const botao = $('button');
+	const select = $('select');
 	let resultado = $('#resultado');
 
-	botao.click(function(){
+	select.change(function(){
+		let moeda = this.value;
+		const api = `https://currencydatafeed.com/api/data.php?token=fs3yuzrl7k8nv1ia2jf1&currency=${moeda}/BRL`; 
+		
 		let valor = $('#valor').val();
 		
 		$.getJSON(api, function(data){
 			let val = data.currency[0].value;
-			let calculo = val*valor;	
+			let calculo = val*valor;
 		
 			resultado.html(`<p>${calculo.toFixed(2)}</p>`);
+			$('#valor').val(null).focus();
+			select[0].selectedIndex = 0;
 		});
+
 	});
 
 });
